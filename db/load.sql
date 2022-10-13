8a04280f9309d5bf2f86a9ba40f17eaa6267bbb0
Luke Triplett <lft4@duke.edu>
2022-10-13 14:12:31 -0400

    added seller table

--- db/load.sql
+++ db/load.sql
@@ -5,6 +5,13 @@ SELECT pg_catalog.setval('public.users_id_seq',
                          (SELECT MAX(id)+1 FROM Users),
                          false);
 
+\COPY Sellers FROM 'Sellers.csv' WITH DELIMITER ',' NULL '' CSV
+-- since id is auto-generated; we need the next command to adjust the counter
+-- for auto-generation so next INSERT will not clash with ids loaded above:
+SELECT pg_catalog.setval('public.sellers_id_seq',
+                         (SELECT MAX(id)+1 FROM Sellers),
+                         false);
+
 \COPY Products FROM 'Products.csv' WITH DELIMITER ',' NULL '' CSV
 SELECT pg_catalog.setval('public.products_id_seq',
                          (SELECT MAX(id)+1 FROM Products),
