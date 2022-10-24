ce5c290f436a83c8329899e190891da23553987e
Muaz Bin Kashif <mb674@duke.edu>
2022-10-23 22:26:57 -0400

    working on purchase views

--- app/getPurchases.py
+++ app/getPurchases.py
@@ -1,4 +1,27 @@
-#!/usr/bin/env python3
+from flask import render_template
+from flask_login import current_user
+import datetime
+
+from .models.product import Product
+from .models.purchase import Purchase
+
+from flask import Blueprint
+bp = Blueprint('purchases', __name__)
+
+
+@bp.route('/purchases/')
+def getPurchases(uid):
+    # find all purchases associated with specific user:
+    purchases = Purchase.get_all_purchases_by_uid()
+    # render the page by adding information to the purchases.html file
+    return render_template('purchases.html', purchase_history=purchases)
+
+
+
+
+
+
+"""#!/usr/bin/env python3
 import psycopg2
 
 def getPurchases(user_id):
@@ -16,4 +39,4 @@ def getPurchases(user_id):
     for uid, sid, pid, number_of_items, total_amount, time_purchased, order_status in cur:
         print('{} {} {} {} {:,.2f} {} {}'.format(uid, sid, pid, number_of_items, total_amount, time_purchased, order_status))
     
-getPurchases(30)
+getPurchases(30) """
--- app/models/purchase.py
+++ app/models/purchase.py
@@ -2,11 +2,13 @@ from flask import current_app as app
 
 
 class Purchase:
-    def __init__(self, id, uid, pid, time_purchased):
-        self.id = id
+    def __init__(self, uid, sid, pid, quant, time_purchased, status):
         self.uid = uid
+        self.sid = sid
         self.pid = pid
+        self.quant = quant
         self.time_purchased = time_purchased
+        self.status = status
 
     @staticmethod
     def get(id):
@@ -29,4 +31,15 @@ ORDER BY time_purchased DESC
 ''',
                               uid=uid,
                               since=since)
+        return [Purchase(*row) for row in rows]
+
+    @staticmethod
+    def get_all_purchases_by_uid(uid):
+        rows = app.db.execute('''
+SELECT *
+FROM Purchases
+WHERE uid = :uid
+ORDER BY time_purchased DESC
+''',
+                              uid=uid)
         return [Purchase(*row) for row in rows]
