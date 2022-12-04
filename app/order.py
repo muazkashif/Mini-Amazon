10741ab6baf6a9ada8e1df7c3be0a33fc5ba911d
Luke Triplett <lft4@duke.edu>
2022-12-04 15:32:00 -0500

    purchase now takes user to most recent order

--- app/order.py
+++ app/order.py
@@ -1,6 +1,6 @@
 from flask import render_template, request
 from flask_login import current_user
-import datetime
+from datetime import datetime
 
 from .models.purchase import Purchase
 from .models.cart import Cart
@@ -31,6 +31,7 @@ def translate(id):
 
 @bp.route('/order/checkout', methods=['GET','POST'])
 def check_out_all():
+    success = False
     cart = Cart.get(current_user.id)
     total_price = 0
     balance = User.get_balance(current_user.id)
@@ -41,7 +42,9 @@ def check_out_all():
         if new_quantity >= 0:
             total_price += float(ForSaleItems.get_price(pid, sid)[0]) * item.quantity
     if total_price < balance:
+        success = True
         User.updateBalance(current_user.id, float(balance)-total_price)
+        time = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
         for item in cart:
             pid, sid, quantity = item.pid, item.sid, item.quantity
             seller_balance = User.get_balance(sid)
@@ -50,13 +53,16 @@ def check_out_all():
             quantity_available = ForSaleItems.get_quantity(int(pid), int(sid))
             new_quantity = quantity_available - int(quantity)
             if new_quantity >= 0:
-                Purchase.add(current_user.id, int(pid), int(sid), int(quantity))
+                Purchase.add(current_user.id, int(pid), int(sid), int(quantity), time)
                 Cart.delete_product_cart(current_user.id, int(pid), int(sid))
                 ForSaleItems.remove(pid, sid, quantity_available, price)
                 ForSaleItems.add(pid, sid, new_quantity, price)
     purchases = Purchase.get_all()
     if current_user.is_authenticated:
-        purchases = Purchase.get_all_purchases_by_uid(current_user.id)
+        if success:
+            purchases = Purchase.get_order(current_user.id, time)
+        else:
+            purchases = Purchase.get_all_purchases_by_uid(current_user.id)
         return render_template('orders.html',
                             purchases=purchases, logged_in=True)
     return render_template('orders.html',
@@ -64,6 +70,7 @@ def check_out_all():
 
 @bp.route('/order/', methods=['GET','POST'])
 def index():
+    success = False
     if request.method == 'POST':
         if current_user.is_authenticated:
             if request.form.get("trash"):
@@ -79,6 +86,8 @@ def index():
                     if new_quantity >= 0:
                         total_price += float(ForSaleItems.get_price(pid, sid)[0]) * int(quantity)
                 if total_price < balance:
+                    success = True
+                    time = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
                     User.updateBalance(current_user.id, float(balance)-total_price)
                     for id in request.form.getlist("selectfromcart"):
                         pid, sid, quantity = translate(id)
@@ -88,13 +97,16 @@ def index():
                         quantity_available = ForSaleItems.get_quantity(int(pid), int(sid))
                         new_quantity = quantity_available - int(quantity)
                         if new_quantity >= 0:
-                            Purchase.add(current_user.id, int(pid), int(sid), int(quantity))
+                            Purchase.add(current_user.id, int(pid), int(sid), int(quantity), time)
                             Cart.delete_product_cart(current_user.id, int(pid), int(sid))
                             ForSaleItems.remove(pid, sid, quantity_available, price)
                             ForSaleItems.add(pid, sid, new_quantity, price)
     purchases = Purchase.get_all()
     if current_user.is_authenticated:
-        purchases = Purchase.get_all_purchases_by_uid(current_user.id)
+        if success:
+            purchases = Purchase.get_order(current_user.id, time)
+        else:
+            purchases = Purchase.get_all_purchases_by_uid(current_user.id)
         return render_template('orders.html',
                             purchases=purchases, logged_in=True)
     return render_template('orders.html',
